351bf4d936ac868795a39579a13bd72c576ec8eb
S. Manna <90498811+codewar193@users.noreply.github.com>
2023-08-18 12:02:27 +0530

    leetCode #medium

---
+++ array./8-Find the Duplicate Number.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& arr) {
+
+        //Approch - 1
+        /*
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i =0;i<n;i++ ){
+            for(int j =i+1;j<n;j++){
+                if(nums[i]==nums[j]){
+                    return nums[i];
+                }
+            }
+        }
+        return -1;
+        */
+     
+    int slow = arr[0];
+	int fast = arr[0];
+
+	do{
+		slow = arr[slow];
+		fast = arr[arr[fast]];
+		
+	} while(slow != fast);
+
+	 fast = arr[0];
+
+	while(slow != fast){
+		slow = arr[slow];
+		fast = arr[fast];
+	}
+
+	return slow;
+
+
+    }
+};
